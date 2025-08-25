03b7dc367e085d2edaaf2303c5ec193251368816
bettuskaaa06-ctrl <bettuskaaa06@gmail.com>
2025-08-25 13:56:06 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
+const CACHE_NAME = 'betti-v1';
+const OFFLINE_URLS = ['/', '/IMAGES/IMG_4972.webp'];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('betti-v1').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/IMAGES/IMG_4972.jpg',
-      '/IMAGES/IMG_4972.webp'
-    ]))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
   );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((resp) => resp || fetch(e.request))
+    caches.match(e.request).then(resp => {
+      const fetchPromise = fetch(e.request).then(networkResp => {
+        const copy = networkResp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+        return networkResp;
+      }).catch(() => resp);
+      return resp || fetchPromise;
+    })
   );
 });
